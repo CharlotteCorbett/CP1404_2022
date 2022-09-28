d8ae6d45d7f82df92070ba34e2c394d064c95b38
Charlotte <charlotte.corbett@my.jcu.edu.au>
2022-09-29 00:28:57 +1000

    Refactor password checking program to utilise functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -11,12 +11,24 @@ For character in user password length:
     Display '*'
 """
 
-password = input("Enter password:")
 
-if len(password) < 7:
-    while len(password) < 7:
-        print("Password too short. Try again.")
-        password = input("Enter password:")
+def main():
+    password = get_valid_password()
+    print_password(password)
 
-for character in password:
-    print('*', end='')
+
+def print_password(password):
+    for character in password:
+        print('*', end='')
+
+
+def get_valid_password():
+    password = input("Enter password:")
+    if len(password) < 7:
+        while len(password) < 7:
+            print("Password too short. Try again.")
+            password = input("Enter password:")
+    return password
+
+
+main()
