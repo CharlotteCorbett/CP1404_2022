5b1c3fa4e53328a41a841eb150774940d9d3ecea
Charlotte <charlotte.corbett@my.jcu.edu.au>
2022-10-05 23:31:05 +1000

    Update finished files.py.

--- prac_03/files.py
+++ prac_03/files.py
@@ -2,6 +2,9 @@
 CP1404 - Practical 3
 This program will demonstrate my solutions to file reading activities given
 in prac_03 README page on GitHub.
+
+Solutions were looked at (When doing Question 3) only for the purpose of checking, and code was edited
+accordingly using the solutions as suggestions for better code practice.
 """
 # Question 1:
 # OUTPUT_FILE = 'name.txt'
@@ -16,3 +19,23 @@ in prac_03 README page on GitHub.
 # out_file.close()
 
 # Question 3:
+# INPUT_FILE = 'numbers.txt'
+# in_file = open(INPUT_FILE, 'r')
+# number_1 = int(in_file.readline())
+# number_2 = int(in_file.readline())
+# total = number_1 + number_2
+# print(total)
+
+# Question 4:
+# INPUT_FILE = 'numbers.txt'
+#
+# in_file = open(INPUT_FILE, 'r')
+#
+# total = 0
+#
+# for line in in_file:
+#     number = int(line)
+#     total += number
+#
+# in_file.close()
+# print(total)
