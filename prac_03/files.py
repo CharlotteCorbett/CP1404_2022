aae1276d04ba68c10369561c867363589c5e0a64
Charlotte <charlotte.corbett@my.jcu.edu.au>
2022-10-05 13:02:42 +1000

    Add files.py, update progress.

---
+++ prac_03/files.py
@@ -0,0 +1,18 @@
+"""
+CP1404 - Practical 3
+This program will demonstrate my solutions to file reading activities given
+in prac_03 README page on GitHub.
+"""
+# Question 1:
+# OUTPUT_FILE = 'name.txt'
+# out_file = open(OUTPUT_FILE, 'w')
+# out_file.write(input("What is your name?"))
+# out_file.close()
+
+# Question 2:
+# OUTPUT_FILE = 'name.txt'
+# out_file = open(OUTPUT_FILE, 'r')
+# print(f"Your name is {out_file.read()}")
+# out_file.close()
+
+# Question 3:
